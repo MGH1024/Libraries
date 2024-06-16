30e4ecf2c0e1038b5ec1f51bd379d94fcbaabf38
meysam ghiasvand <meysam.ghiasvand@hotmail.com>
2024-06-16 12:33:25 +0330

    refactor in library repository

--- Microservices/Library/src/Domain/Library.Domain/Entities/Libraries/ILibraryRepository.cs
+++ Microservices/Library/src/Domain/Library.Domain/Entities/Libraries/ILibraryRepository.cs
@@ -9,7 +9,6 @@ namespace Domain.Entities.Libraries;
 
 public interface ILibraryRepository : IQuery<Library>
 {
-    
     Task<Library> GetAsync(
         Expression<Func<Library, bool>> predicate,
         Func<IQueryable<Library>, IIncludableQueryable<Library, object>> include = null,
@@ -17,7 +16,6 @@ public interface ILibraryRepository : IQuery<Library>
         bool enableTracking = true,
         CancellationToken cancellationToken = default
     );
-
     Task<IPaginate<Library>> GetListAsync(
         Expression<Func<Library, bool>> predicate = null,
         Func<IQueryable<Library>, IOrderedQueryable<Library>> orderBy = null,
@@ -28,34 +26,7 @@ public interface ILibraryRepository : IQuery<Library>
         bool enableTracking = true,
         CancellationToken cancellationToken = default
     );
-
-    Task<IPaginate<Library>> GetListByDynamicAsync(
-        DynamicQuery dynamic,
-        Expression<Func<Library, bool>> predicate = null,
-        Func<IQueryable<Library>, IIncludableQueryable<Library, object>> include = null,
-        int index = 0,
-        int size = 10,
-        bool withDeleted = false,
-        bool enableTracking = true,
-        CancellationToken cancellationToken = default
-    );
-
-    Task<bool> AnyAsync(
-        Expression<Func<Library, bool>> predicate = null,
-        bool withDeleted = false,
-        bool enableTracking = true,
-        CancellationToken cancellationToken = default
-    );
-
     Task<Library> AddAsync(Library entity,CancellationToken cancellationToken);
-
-    Task<ICollection<Library>> AddRangeAsync(ICollection<Library> entity);
-
-    Library Update(Library entity);
-
-    ICollection<Library> UpdateRange(ICollection<Library> entity);
-
     Task<Library> DeleteAsync(Library entity, bool permanent = false);
-
-    Task<ICollection<Library>> DeleteRangeAsync(ICollection<Library> entity, bool permanent = false);
+    
 }
--- Microservices/Library/src/Infrustructure/Library.Persistence/Repositories/LibraryRepository.cs
+++ Microservices/Library/src/Infrustructure/Library.Persistence/Repositories/LibraryRepository.cs
@@ -58,7 +58,7 @@ public class LibraryRepository(LibraryDbContext libraryDbContext) : ILibraryRepo
         return await queryable.ToPaginateAsync(index, size, from: 0, cancellationToken);
     }
 
-    public async Task<IPaginate<Library>> GetListByDynamicAsync(
+    public async Task<IPaginate<Library>> GetDynamicListAsync(
         DynamicQuery dynamic,
         Expression<Func<Library, bool>> predicate = null,
         Func<IQueryable<Library>, IIncludableQueryable<Library, object>> include = null,
@@ -79,58 +79,17 @@ public class LibraryRepository(LibraryDbContext libraryDbContext) : ILibraryRepo
             queryable = queryable.Where(predicate);
         return await queryable.ToPaginateAsync(index, size, from: 0, cancellationToken);
     }
-
-    public async Task<bool> AnyAsync(
-        Expression<Func<Library, bool>> predicate = null,
-        bool withDeleted = false, 
-        bool enableTracking = true,
-        CancellationToken cancellationToken = default)
-    {
-        IQueryable<Library> queryable = Query();
-        if (!enableTracking)
-            queryable = queryable.AsNoTracking();
-        if (withDeleted)
-            queryable = queryable.IgnoreQueryFilters();
-        if (predicate != null)
-            queryable = queryable.Where(predicate);
-        return await queryable.AnyAsync(cancellationToken);
-    }
-
+    
     public async Task<Library> AddAsync(Library entity, CancellationToken cancellationToken)
     {
         await libraryDbContext.AddAsync(entity, cancellationToken);
         return entity;
     }
-
-    public async Task<ICollection<Library>> AddRangeAsync(ICollection<Library> entity)
-    {
-        await libraryDbContext.AddRangeAsync(entity);
-        return entity;
-    }
-
-    public Library  Update(Library entity)
-    {
-        libraryDbContext.Update(entity);
-        return entity;
-    }
-
-    public ICollection<Library> UpdateRange(ICollection<Library> entity)
-    {
-        libraryDbContext.UpdateRange(entity);
-        return entity;
-    }
-
     public async Task<Library> DeleteAsync(Library entity, bool permanent = false)
     {
         await SetEntityAsDeletedAsync(entity, permanent);
         return entity;
     }
-
-    public Task<ICollection<Library>> DeleteRangeAsync(ICollection<Library> entity, bool permanent = false)
-    {
-        throw new NotImplementedException();
-    }
-
     private async Task SetEntityAsDeletedAsync(Library entity, bool permanent)
     {
         if (!permanent)
@@ -143,7 +102,6 @@ public class LibraryRepository(LibraryDbContext libraryDbContext) : ILibraryRepo
             libraryDbContext.Remove(entity);
         }
     }
-
     private void CheckHasEntityHaveOneToOneRelation(Library entity)
     {
         bool hasEntityHaveOneToOneRelation =
@@ -162,7 +120,6 @@ public class LibraryRepository(LibraryDbContext libraryDbContext) : ILibraryRepo
                 " if you try to create entry again by same foreign key."
             );
     }
-
     private async Task SetEntityAsSoftDeletedAsync(IAuditable entity)
     {
         if (entity.DeletedAt.HasValue)
@@ -215,7 +172,6 @@ public class LibraryRepository(LibraryDbContext libraryDbContext) : ILibraryRepo
 
         libraryDbContext.Update(entity);
     }
-
     private IQueryable<object> GetRelationLoaderQuery(IQueryable query, Type navigationPropertyType)
     {
         var queryProviderType = query.Provider.GetType();
