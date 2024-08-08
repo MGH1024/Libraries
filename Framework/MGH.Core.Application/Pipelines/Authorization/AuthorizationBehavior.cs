6d776f1dfc617197d635bb7296d62a55d7d3c46b
MGH1024 <meysam.ghiasvand@gmail.com>
2024-08-09 00:40:21 +0330

    update packages

--- Framework/MGH.Core.Application/Pipelines/Authorization/AuthorizationBehavior.cs
+++ Framework/MGH.Core.Application/Pipelines/Authorization/AuthorizationBehavior.cs
@@ -3,32 +3,25 @@ using MGH.Core.CrossCutting.Exceptions.Types;
 using MGH.Core.Infrastructure.Securities.Security.Constants;
 using MGH.Core.Infrastructure.Securities.Security.Extensions;
 using Microsoft.AspNetCore.Http;
-using Microsoft.IdentityModel.Tokens;
 
 namespace MGH.Core.Application.Pipelines.Authorization;
 
-public class AuthorizationBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
+public class AuthorizationBehavior<TRequest, TResponse>(IHttpContextAccessor httpContextAccessor)
+    : IPipelineBehavior<TRequest, TResponse>
     where TRequest : IRequest<TResponse>, ISecuredRequest
 {
-    private readonly IHttpContextAccessor _httpContextAccessor;
-
-    public AuthorizationBehavior(IHttpContextAccessor httpContextAccessor)
-    {
-        _httpContextAccessor = httpContextAccessor;
-    }
-
     public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
     {
-        List<string> userRoleClaims = _httpContextAccessor.HttpContext?.User.ClaimRoles();
+        List<string> userRoleClaims = httpContextAccessor.HttpContext?.User.ClaimRoles();
 
         if (userRoleClaims == null)
             throw new AuthorizationException("You are not authenticated.");
 
-        bool isNotMatchedAUserRoleClaimWithRequestRoles = userRoleClaims
+        bool isNotMatchedAUserRoleClaimWithRequestRoles = string.IsNullOrEmpty(userRoleClaims
             .FirstOrDefault(
                 userRoleClaim => userRoleClaim == GeneralOperationClaims.Admin || 
                                  request.Roles.Any(role => role == userRoleClaim)
-            ).IsNullOrEmpty();
+            ));
         
         if (isNotMatchedAUserRoleClaimWithRequestRoles)
             throw new AuthorizationException("You are not authorized.");
