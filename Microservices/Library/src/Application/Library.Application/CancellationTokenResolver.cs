84cd811739791aaafd616500158ce2517ba012b9
MGH1024 <meysam.ghiasvand@gmail.com>
2024-08-16 02:20:19 +0330

    Login command refactor

---
+++ Microservices/Library/src/Application/Library.Application/CancellationTokenResolver.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+
+namespace MGH.Core.Application.Extensions;
+
+public class CancellationTokenResolver<TSource, TDestination> : IValueResolver<TSource, TDestination, CancellationToken>
+{
+    public CancellationToken Resolve(TSource source, TDestination destination, CancellationToken destMember, ResolutionContext context)
+    {
+        return context.Items.TryGetValue("CancellationToken", out var token) && token is CancellationToken ct
+            ? ct
+            : CancellationToken.None;
+    }
+}
