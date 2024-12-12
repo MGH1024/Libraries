84f979347c67459ee3ad0758e50435c27a1d73f3
MGH1024 <meysam.ghiasvand@gmail.com>
2024-12-13 00:46:23 +0330

    bugfix

--- Microservices/Library/src/Library.Application/Features/OutBoxes/Queries/GetList/GetOutboxListDto.cs
+++ Microservices/Library/src/Library.Application/Features/OutBoxes/Queries/GetList/GetOutboxListDto.cs
@@ -1,3 +1,31 @@
 namespace Application.Features.OutBoxes.Queries.GetList;
 
-public record GetOutboxListDto(Guid Id, string Type, string Content, DateTime CreatedAt, DateTime? ProcessedAt, string Error);
+public class GetOutboxListDto
+{
+    public Guid Id { get; set; }
+    public string Type { get; set; }
+    public string Content { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public DateTime? ProcessedAt { get; set; }
+    public string Error { get; set; }
+
+    public GetOutboxListDto()
+    {
+        
+    }
+    public GetOutboxListDto(
+        Guid id,
+        string type,
+        string content,
+        DateTime createdAt,
+        DateTime? processedAt,
+        string error)
+    {
+        Id = id;
+        Type = type;
+        Content = content;
+        CreatedAt = createdAt;
+        ProcessedAt = processedAt;
+        Error = error;
+    }
+}
