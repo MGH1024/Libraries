b2501802954a3fb364c4bedccef8987222e9804e
MGH1024 <meysam.ghiasvand@gmail.com>
2024-12-30 21:11:30 +0330

    add migration add lending table

---
+++ Microservices/Library/src/Library.Infrastructure/Migrations/20241230170027_AddLending.cs
@@ -0,0 +1,236 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Library.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddLending : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "BookId",
+                table: "DomainEvent",
+                type: "uniqueidentifier",
+                nullable: true);
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "LendingId",
+                table: "DomainEvent",
+                type: "uniqueidentifier",
+                nullable: true);
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "MemberId",
+                table: "DomainEvent",
+                type: "uniqueidentifier",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "Books",
+                schema: "lib",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Isbn = table.Column<string>(type: "nvarchar(13)", maxLength: 13, nullable: false),
+                    Title = table.Column<string>(type: "nvarchar(256)", maxLength: 256, nullable: false),
+                    UniqueCode = table.Column<string>(type: "nvarchar(32)", maxLength: 32, nullable: false),
+                    IsReference = table.Column<bool>(type: "bit", nullable: false),
+                    PublicationDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Version = table.Column<int>(type: "int", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    CreatedBy = table.Column<string>(type: "nvarchar(64)", maxLength: 64, nullable: true),
+                    CreatedByIp = table.Column<string>(type: "nvarchar(64)", maxLength: 64, nullable: true),
+                    UpdatedAt = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    UpdatedBy = table.Column<string>(type: "nvarchar(64)", maxLength: 64, nullable: true),
+                    UpdatedByIp = table.Column<string>(type: "nvarchar(64)", maxLength: 64, nullable: true),
+                    DeletedAt = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    DeletedBy = table.Column<string>(type: "nvarchar(64)", maxLength: 64, nullable: true),
+                    DeletedByIp = table.Column<string>(type: "nvarchar(64)", maxLength: 64, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Books", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Lendings",
+                schema: "lib",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    BookId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    LibraryId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    MemberId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    LendingDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    ReturnDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Version = table.Column<int>(type: "int", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    CreatedBy = table.Column<string>(type: "nvarchar(64)", maxLength: 64, nullable: true),
+                    CreatedByIp = table.Column<string>(type: "nvarchar(64)", maxLength: 64, nullable: true),
+                    UpdatedAt = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    UpdatedBy = table.Column<string>(type: "nvarchar(64)", maxLength: 64, nullable: true),
+                    UpdatedByIp = table.Column<string>(type: "nvarchar(64)", maxLength: 64, nullable: true),
+                    DeletedAt = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    DeletedBy = table.Column<string>(type: "nvarchar(64)", maxLength: 64, nullable: true),
+                    DeletedByIp = table.Column<string>(type: "nvarchar(64)", maxLength: 64, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Lendings", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Members",
+                schema: "lib",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    FullName = table.Column<string>(type: "nvarchar(128)", maxLength: 128, nullable: false),
+                    NationalCode = table.Column<string>(type: "nvarchar(10)", maxLength: 10, nullable: false),
+                    MobileNumber = table.Column<string>(type: "nvarchar(13)", maxLength: 13, nullable: false),
+                    Address = table.Column<string>(type: "nvarchar(1024)", maxLength: 1024, nullable: false),
+                    Version = table.Column<int>(type: "int", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    CreatedBy = table.Column<string>(type: "nvarchar(64)", maxLength: 64, nullable: true),
+                    CreatedByIp = table.Column<string>(type: "nvarchar(64)", maxLength: 64, nullable: true),
+                    UpdatedAt = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    UpdatedBy = table.Column<string>(type: "nvarchar(64)", maxLength: 64, nullable: true),
+                    UpdatedByIp = table.Column<string>(type: "nvarchar(64)", maxLength: 64, nullable: true),
+                    DeletedAt = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    DeletedBy = table.Column<string>(type: "nvarchar(64)", maxLength: 64, nullable: true),
+                    DeletedByIp = table.Column<string>(type: "nvarchar(64)", maxLength: 64, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Members", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Authors",
+                schema: "lib",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    FullName = table.Column<string>(type: "nvarchar(128)", maxLength: 128, nullable: false),
+                    NationalCode = table.Column<string>(type: "nvarchar(11)", maxLength: 11, nullable: false),
+                    BookId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Authors", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Authors_Books_BookId",
+                        column: x => x.BookId,
+                        principalSchema: "lib",
+                        principalTable: "Books",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DomainEvent_BookId",
+                table: "DomainEvent",
+                column: "BookId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DomainEvent_LendingId",
+                table: "DomainEvent",
+                column: "LendingId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DomainEvent_MemberId",
+                table: "DomainEvent",
+                column: "MemberId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Authors_BookId",
+                schema: "lib",
+                table: "Authors",
+                column: "BookId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_DomainEvent_Books_BookId",
+                table: "DomainEvent",
+                column: "BookId",
+                principalSchema: "lib",
+                principalTable: "Books",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_DomainEvent_Lendings_LendingId",
+                table: "DomainEvent",
+                column: "LendingId",
+                principalSchema: "lib",
+                principalTable: "Lendings",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_DomainEvent_Members_MemberId",
+                table: "DomainEvent",
+                column: "MemberId",
+                principalSchema: "lib",
+                principalTable: "Members",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_DomainEvent_Books_BookId",
+                table: "DomainEvent");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_DomainEvent_Lendings_LendingId",
+                table: "DomainEvent");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_DomainEvent_Members_MemberId",
+                table: "DomainEvent");
+
+            migrationBuilder.DropTable(
+                name: "Authors",
+                schema: "lib");
+
+            migrationBuilder.DropTable(
+                name: "Lendings",
+                schema: "lib");
+
+            migrationBuilder.DropTable(
+                name: "Members",
+                schema: "lib");
+
+            migrationBuilder.DropTable(
+                name: "Books",
+                schema: "lib");
+
+            migrationBuilder.DropIndex(
+                name: "IX_DomainEvent_BookId",
+                table: "DomainEvent");
+
+            migrationBuilder.DropIndex(
+                name: "IX_DomainEvent_LendingId",
+                table: "DomainEvent");
+
+            migrationBuilder.DropIndex(
+                name: "IX_DomainEvent_MemberId",
+                table: "DomainEvent");
+
+            migrationBuilder.DropColumn(
+                name: "BookId",
+                table: "DomainEvent");
+
+            migrationBuilder.DropColumn(
+                name: "LendingId",
+                table: "DomainEvent");
+
+            migrationBuilder.DropColumn(
+                name: "MemberId",
+                table: "DomainEvent");
+        }
+    }
+}
